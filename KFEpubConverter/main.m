f431d9ce8c8fd49f9b96e4c6207caa4d8d6a4a03
rico becker <rico.becker@kf-interactive.com>
2013-08-19 09:30:23 +0200

    Initial Commit

---
+++ KFEpubConverter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  KFEpubConverter
+//
+//  Created by rick on 19.08.13.
+//  Copyright (c) 2013 KF Interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
